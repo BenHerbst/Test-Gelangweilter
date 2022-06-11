1e3ca28c380ca04b1e923079a0f9f851ebae9494
Florian <admin@Durchzocken.net>
2022-06-12 00:14:46 +0200

    Next try

--- src/main/kotlin/module-info.java
+++ src/main/kotlin/module-info.java
@@ -1,21 +1,3 @@
-module idaesbasic.org.idaesbasic {
-    requires javafx.base;
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires tornadofx;
-    requires org.fxmisc.richtext;
-    requires kotlin.stdlib;
-    requires org.kordamp.ikonli.core;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.ikonli.fontawesome;
-    requires tornadofx.controlsfx;
-    requires wellbehavedfx;
+module Test.de.medienproduktion.test {
 
-    opens org.idaesbasic to tornadofx;
-    opens org.idaesbasic.buffer to tornadofx;
-    opens org.idaesbasic.buffer.run to tornadofx;
-    opens org.idaesbasic.sidepanel to tornadofx;
-    opens org.idaesbasic.powerline to tornadofx;
-
-    exports org.idaesbasic;
 }
